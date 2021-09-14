94d81ccdea1198abbc8c572ea7aa5504a3724807
httpmurilo <muriloeduardo1997@outlook.com>
2021-09-13 21:03:09 -0300

    Continuação projeto

--- src/main/java/curso/springboot/spring/controller/PessoaController.java
+++ src/main/java/curso/springboot/spring/controller/PessoaController.java
@@ -4,9 +4,13 @@ import curso.springboot.spring.dao.IPessoaDao;
 import curso.springboot.spring.model.Pessoa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Controller
 public class PessoaController {
@@ -15,14 +19,20 @@ public class PessoaController {
     private IPessoaDao pessoaDao;
 
     @RequestMapping(method = RequestMethod.GET, value = "/cadastro-pessoa")
-    public String inicio() {
-        return "cadastro/cadastro-pessoa";
+    public ModelAndView inicio() {
+        ModelAndView objView = new ModelAndView("cadastro/cadastro-pessoa");
+        objView.addObject("pessoaobj", new Pessoa());
+        return objView;
     }
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/salvar-pessoa")
-    public String salvarRegistro(Pessoa pessoa) {
+    public ModelAndView salvarRegistro(Pessoa pessoa) {
         pessoaDao.save(pessoa);
-        return "cadastro/cadastro-pessoa";
+        ModelAndView objView = new ModelAndView("cadastro/cadastro-pessoa");
+        Iterable<Pessoa> pessoasIt = pessoaDao.findAll();
+        objView.addObject("pessoas",pessoasIt);
+        objView.addObject("pessoaobj", new Pessoa());
+        return objView;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/pessoas")
@@ -30,6 +40,15 @@ public class PessoaController {
         ModelAndView objView = new ModelAndView("cadastro/cadastro-pessoa");
         Iterable<Pessoa> pessoasIt = pessoaDao.findAll();
         objView.addObject("pessoas",pessoasIt);
+        objView.addObject("pessoaobj", new Pessoa());
+        return objView;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/editar-pessoa/{idpessoa}")
+    public ModelAndView editarPessoa(@PathVariable("idpessoa") Long idpessoa){
+        ModelAndView objView = new ModelAndView("cadastro/cadastro-pessoa");
+        Optional<Pessoa> pessoa = pessoaDao.findById(idpessoa);
+        objView.addObject("pessoaobj", pessoa.get());
         return objView;
     }
 }
