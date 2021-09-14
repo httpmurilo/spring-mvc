8a3f7f26c2acf9f7ffac3608ffb5aff3feab991a
httpmurilo <muriloeduardo1997@outlook.com>
2021-09-14 20:06:03 -0300

    ajuste front

--- src/main/java/curso/springboot/spring/controller/PessoaController.java
+++ src/main/java/curso/springboot/spring/controller/PessoaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.swing.text.html.Option;
@@ -59,5 +60,14 @@ public class PessoaController {
         objView.addObject("pessoas", pessoaDao.findAll());
         objView.addObject("pessoaobj", new Pessoa());
         return objView;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/pesquisar-pessoa")
+    public ModelAndView pesquisarPessoa(@RequestParam("nomepesquisa") String nomepesquisa) {
+        ModelAndView objView = new ModelAndView("cadastro/cadastro-pessoa");
+        objView.addObject("pessoas",pessoaDao.buscarPessoaPorNome(nomepesquisa));
+        objView.addObject("pessoaobj", new Pessoa());
+        return  objView;
+
     }
 }
--- src/main/java/curso/springboot/spring/dao/IPessoaDao.java
+++ src/main/java/curso/springboot/spring/dao/IPessoaDao.java
@@ -1,11 +1,17 @@
 package curso.springboot.spring.dao;
 
 import curso.springboot.spring.model.Pessoa;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 @Transactional
 public interface IPessoaDao extends CrudRepository<Pessoa, Long> {
+
+    @Query("select p from Pessoa p where p.nome like %?1%")
+    List<Pessoa> buscarPessoaPorNome(String nome);
 }
